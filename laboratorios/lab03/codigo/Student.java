334d41b66524f30a24663ddca87edaa73f922809
clopezr9 <46933189+clopezr9@users.noreply.github.com>
2019-03-24 18:47:44 -0500

    Update Student.java

--- laboratorios/lab03/codigo/Student.java
+++ laboratorios/lab03/codigo/Student.java
@@ -9,15 +9,34 @@ public class Student
 {
     // instance variables - replace the example below with your own
     public Pair<Integer, Integer> grades;
+    public int code;
+    public List<Subject> studentSubjects;
     
     /**
      * Constructor for objects of class Student
      * @param code
      */
-    public Student(Pair<Integer, Integer> grades)
+    public Student(Pair<Integer, Integer> grades,int code, List<Subject> studentSubjects)
     {
         // initialise instance variables
         this.grades = grades;
+        this.code = code;
+        this.studentSubjects = studentSubjects;
     }
-
+    
+    public int getCode(){
+        return code;
+    }
+    
+    public List<Subject> getSubjects(){
+        return studentSubjects;
+    }
+    
+    public int getGrade(String subjectCode, int grade){
+        for(Subject sub: this.studentSubjects){
+            if(subjectCode.equals(getCode())){
+               return grade;
+            }
+        }
+    }
 }

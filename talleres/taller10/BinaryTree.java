c1c65cecf49d86e4d0f197f0202c9fdf6f2bb482
clopezr9 <46933189+clopezr9@users.noreply.github.com>
2019-04-03 11:21:29 -0500

    Add files via upload

---
+++ talleres/taller10/BinaryTree.java
@@ -0,0 +1,98 @@
+/**
+ * La clase BinaryTree intenta dar un conocimiento a los
+ * estudiantes acerca del manejo de un arbol binario de enteros.  
+ * Aquí se espera crear una estrcutura concreta del árbol binario,
+ * cabe aclarar que esta estrucutra ya esta implementada.
+ * @see <a href="https://docs.oracle.com/javase/9/docs/api/jdk/nashorn/api/tree/BinaryTree.html"> Árbol Binario</a>
+ * @author clopezr9, adchavesp
+ * 
+ */
+
+public class BinaryTree {
+
+    // Sirve como raíz del árbol
+    public Node root;
+
+    public BinaryTree (int data){
+        this.root = new Node (data);
+    }
+
+    public void setRoot(Node root){
+        this.root = root;
+    }
+
+    public Node setNode(int data){
+        Node node = new Node(data);
+        return node;
+    }
+
+    /**
+     * @param n el dato del nodo que se busca buscar
+     * Metodo auxiliar de insetarAux.
+     *
+     */
+    public void insertar(int n) {
+        insertarAux(root, n);
+    }
+
+    /**
+     * @param node es la raíz del arbol
+     * @param n el data del nodo que se busca insertar
+     * nota: metodo recursivo.
+     * Inserta un dato respetando claro las desigualdades en el árbol
+     */
+    private void insertarAux(Node node, int n) { //O(n)=O(1)
+        if(node == null){
+            this.setRoot(setNode(n));
+        } else if(node.getData() >= n){
+            if(node.getLeft() == null){
+                node.setLeft(setNode(n));
+            } else 
+                insertarAux(node.getLeft(),n);
+        }else {
+            if(node.getRight() == null){
+                node.setRight(setNode(n));
+            } else {
+                insertarAux(node.getRight(), n);
+            }
+        }// https://serprogramador.es/programar-arboles-binarios-parte-1-introduccionclasesagregar-nodo/
+    }
+
+    /**
+     * @param n el dato del nodo que se busca.
+     * Metodo auxiliar de buscarAux.
+     * @return true si el metodo buscarAux es true
+     */
+    public boolean buscar(int n) {
+        return buscarAux(root, n);
+    }
+
+    /**
+     * @param node es la raíz del arbol
+     * @param n el data del nodo que se busca
+     * nota: metodo recursivo.
+     * Inserta un dato respetando claro las desigualdades en el árbol
+     * @return true si se encuentra el elemento en el árbol, false de lo contrario
+     */ 
+
+    private boolean buscarAux(Node node, int n) { //O(n)=O(1)
+
+        if(node == null){
+            return false;
+        }else if(node.getData() == n){
+            return true;
+        }
+
+        if(node.data > n){
+            return buscarAux(node.getLeft(), n);
+        } else if (node.data < n){
+            return buscarAux(node.getRight(), n);
+        } else {
+
+            return false;
+        }
+    }
+
+
+ 
+}
---
+++ talleres/taller10/Node.java
@@ -0,0 +1,31 @@
+
+public class Node {
+    public Node left;
+    public Node right;
+    public int data;
+
+    public Node(int d){ 
+        this.data = d;
+    }
+    
+    
+    public void setRight(Node right){
+        this.right=right;
+    }
+    
+    public void setLeft(Node left){
+        this.left = left;
+    }
+
+    public Node getLeft(){
+        return left;
+    }
+
+    public Node getRight(){
+        return right;
+    }
+    
+    public int getData(){
+        return data;
+    }
+}
